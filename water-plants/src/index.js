35200a4ef6bc07383b28100165863b03288eb4f9
JRodDvlpr <Mialienproductions@gmail.com>
2020-02-26 21:47:53 -0500

    index updated

--- water-plants/src/index.js
+++ water-plants/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//Redux
+// import { Provider } from 'react-redux';
+// import { ConnectedRouter } from 'connected-react-router';
+// import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+    //setting up redux store & provider
+
+    <App />, 
+    
+    rootElement);
+
