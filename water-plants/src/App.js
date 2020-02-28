2bb2eae588461294f65db0c55d103f7d07263624
Frances Hampton <bethhampton@Francess-MBP.attlocal.net>
2020-02-27 20:33:33 -0600

    App.js-RouteDone-francesHampton

--- water-plants/src/App.js
+++ water-plants/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import Header from "./components/Header.js";
+import Login from "./components/Login.js";
+import { Route, Link } from "react-router-dom";
+import Register from "./components/Register.js";
+
 
 import Nav from './components/Nav';
 
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Nav />
-    </div>
+    <main>
+      <Nav>
+        <Link className="navLinks" to="/login">Login</Link>
+        <Link className="navLinks" to="/signup">Sign Up</Link>
+      </Nav>
+
+      <Header />
+      <Route exact path="/" component={Login} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Register} />
+    </main>
   );
 }
-
-export default App;
