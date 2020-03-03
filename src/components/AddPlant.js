31590c8c8e9c74e0468e3362b3a88e4cf75bf0ef
Frances Hampton <bethhampton@Francess-MBP.attlocal.net>
2020-03-03 14:25:22 -0600

    AddPlant.js ready-francesHampton

--- src/components/AddPlant.js
+++ src/components/AddPlant.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import Drawer from './Drawer';
+import Drawer from "./Drawer";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { addPlant } from "../store/actions";
 
 const StyledContainer = styled.div`
     width: 475px;
@@ -10,7 +12,7 @@ const StyledContainer = styled.div`
     margin-top: 2.4%;
     margin-right: 5%;
     height: 600px;
-  `;
+`;
 const StyledInputContainer = styled.div`
     display: flex;
     flex-direction: column;
@@ -18,8 +20,7 @@ const StyledInputContainer = styled.div`
     padding-left: 2%;
     padding-right: 5%;
     padding-bottom: 5%;
-  `;
-
+`;
 const StyledH1 = styled.h1`
     font-size: 1rem;
     text-align: left;
@@ -39,14 +40,13 @@ const StyledLIDiv = styled.div`
     width: 100%;
     margin-bottom: 1%;
     align-items: flex-end;
-  `;
-
+`;
 const Styledlabel = styled.label`
     display: flex;
     justify-content: space-between;
     font-size: 14px;
     color: #7ea897;
-  `;
+`;
 const StyledAddbtn = styled.button`
     color: white;
     background: #78c885;
@@ -59,8 +59,7 @@ const StyledAddbtn = styled.button`
     margin-right: 5px;
     margin-bottom: 10px;
     cursor: pointer;
-  `;
-
+`;
 const StyledCancelbtn = styled.button`
     color: white;
     background: #78c885;
@@ -73,43 +72,38 @@ const StyledCancelbtn = styled.button`
     margin-right: 5px;
     margin-bottom: 10px;
     cursor: pointer;
-  `;
-
+`;
 const StyledBtnDiv = styled.div`
     margin-left: 110px;
-  `;
-
-
+`;
 const AddPlant = props => {
+    const dispatch = useDispatch();
     const [input, setInput] = useState({
         nickname: "",
         species: "",
         h2o_frequency: "",
+        image: '',
     });
-
     // Form Handlers
-
     const inputHandler = event => {
+        //add this
+        event.preventDefault();
         setInput({ ...input, [event.target.name]: event.target.value });
         console.log(event.target.name)
     };
-
     const submitHandler = event => {
         event.preventDefault();
-        props.add({ ...input, id: Math.random() });
-        setInput({ nickname: "", species: "", h2o_frequency: "" });
+        dispatch(addPlant(input));
+        setInput({ nickname: "", species: "", h2o_frequency: "", image: "" });
+        console.log(input)
     };
-
     // Form
-
-
     return (
         <>
             <Drawer />
             <StyledContainer>
                 <form onSubmit={submitHandler} className="form">
                     <StyledH1>Add Plant</StyledH1>
-
                     <StyledInputContainer className="input-container">
                         {/* Name of plant input */}
                         <StyledLIDiv>
@@ -118,12 +112,9 @@ const AddPlant = props => {
                                     type="text"
                                     value={input.nickname}
                                     onChange={(event) => inputHandler(event)}
-                                    name="nickname"
-                                />
+                                    name="nickname" />
                             </Styledlabel>
-
                         </StyledLIDiv>
-
                         {/* Species of plant input */}
                         <StyledLIDiv>
                             <Styledlabel htmlFor="species">Species: </Styledlabel>
@@ -134,20 +125,18 @@ const AddPlant = props => {
                                 name="species"
                             />
                         </StyledLIDiv>
-
                         {/* H2oFrequency of plant */}
                         <StyledLIDiv>
                             <Styledlabel htmlFor="h2o_frequency">H2o Frequency: </Styledlabel>
                             <StyledInput
                                 type="text"
-                                value={input["h2o_frequency"]}
+                                value={input.h2o_frequency}
                                 onChange={inputHandler}
                                 name="h2o_frequency"
                             />
                         </StyledLIDiv>
                     </StyledInputContainer>
                 </form>
-
                 <StyledBtnDiv>
                     <StyledCancelbtn>Cancel</StyledCancelbtn>
                     <StyledAddbtn onClick={submitHandler}>Add</StyledAddbtn>
@@ -156,5 +145,4 @@ const AddPlant = props => {
         </>
     );
 };
-
 export default AddPlant;
