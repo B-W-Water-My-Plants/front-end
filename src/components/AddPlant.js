141fba8280651125ea9f7b6f241fdae7d4380733
JRodDvlpr <Mialienproductions@gmail.com>
2020-03-03 20:04:56 -0500

    :bug: Fix

--- src/components/AddPlant.js
+++ src/components/AddPlant.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import Drawer from "./Drawer";
 import styled from "styled-components";
+
+
 import { useDispatch } from "react-redux";
+
 import { addPlant } from "../store/actions";
-<<<<<<< HEAD
+
+
 
 
 const StyledContainer = styled.div`
@@ -138,6 +142,7 @@ const AddPlant = props => {
                     </StyledLIDiv>
 
                     {/* Species of plant input */}
+                    
                     <StyledLIDiv>
                         <Styledlabel htmlFor="species">Species: </Styledlabel>
                         <StyledInput
@@ -168,149 +173,6 @@ const AddPlant = props => {
         </StyledContainer>
         </>
     );
-};
-=======
->>>>>>> 3d388f5a203f1f9570089cd31271f686250d1922
 
-const StyledContainer = styled.div`
-    width: 475px;
-    background-color: #E8DEBD;
-    margin: 0 auto;
-    border-radius: 15px;
-    margin-top: 2.4%;
-    margin-right: 5%;
-    height: 600px;
-`;
-const StyledInputContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding-left: 2%;
-    padding-right: 5%;
-    padding-bottom: 5%;
-`;
-const StyledH1 = styled.h1`
-    font-size: 1rem;
-    text-align: left;
-    padding-left: 6%;
-    padding-top: 5%;
-    padding-bottom: 3%;
-    color: #198974;
-  `;
-const StyledInput = styled.input`
-    width: 77%;
-    border-radius: 4px;
-    height: 20px;
-  `;
-const StyledLIDiv = styled.div`
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    margin-bottom: 1%;
-    align-items: flex-end;
-`;
-const Styledlabel = styled.label`
-    display: flex;
-    justify-content: space-between;
-    font-size: 14px;
-    color: #7ea897;
-`;
-const StyledAddbtn = styled.button`
-    color: white;
-    background: #78c885;
-    border: none;
-    font-size: 13px;
-    border-radius: 3px;
-    outline: none;
-    padding: 10px 30px 10px 30px;
-    margin-top: 10px;
-    margin-right: 5px;
-    margin-bottom: 10px;
-    cursor: pointer;
-`;
-const StyledCancelbtn = styled.button`
-    color: white;
-    background: #78c885;
-    border: none;
-    font-size: 13px;
-    border-radius: 3px;
-    outline: none;
-    padding: 10px 25px 10px 25px;
-    margin-top: 10px;
-    margin-right: 5px;
-    margin-bottom: 10px;
-    cursor: pointer;
-`;
-const StyledBtnDiv = styled.div`
-    margin-left: 110px;
-`;
-const AddPlant = props => {
-    const dispatch = useDispatch();
-    const [input, setInput] = useState({
-        nickname: "",
-        species: "",
-        h2o_frequency: "",
-        image: '',
-    });
-    // Form Handlers
-    const inputHandler = event => {
-        //add this
-        event.preventDefault();
-        setInput({ ...input, [event.target.name]: event.target.value });
-        console.log(event.target.name)
-    };
-    const submitHandler = event => {
-        event.preventDefault();
-        dispatch(addPlant(input));
-        setInput({ nickname: "", species: "", h2o_frequency: "", image: "" });
-        console.log(input)
-    };
-    // Form
-    return (
-        <>
-            <Drawer />
-            <StyledContainer>
-                <form onSubmit={submitHandler} className="form">
-                    <StyledH1>Add Plant</StyledH1>
-                    <StyledInputContainer className="input-container">
-                        {/* Name of plant input */}
-                        <StyledLIDiv>
-                            <Styledlabel htmlFor="nickname">
-                                Nickname: <StyledInput
-                                    type="text"
-                                    value={input.nickname}
-                                    onChange={(event) => inputHandler(event)}
-                                    name="nickname" />
-                            </Styledlabel>
-                        </StyledLIDiv>
-                        {/* Species of plant input */}
-                        <StyledLIDiv>
-                            <Styledlabel htmlFor="species">Species: </Styledlabel>
-                            <StyledInput
-                                type="text"
-                                value={input.species}
-                                onChange={inputHandler}
-                                name="species"
-                            />
-                        </StyledLIDiv>
-                        {/* H2oFrequency of plant */}
-                        <StyledLIDiv>
-                            <Styledlabel htmlFor="h2o_frequency">H2o Frequency: </Styledlabel>
-                            <StyledInput
-                                type="text"
-                                value={input.h2o_frequency}
-                                onChange={inputHandler}
-                                name="h2o_frequency"
-                            />
-                        </StyledLIDiv>
-                    </StyledInputContainer>
-                </form>
-                <StyledBtnDiv>
-                    <StyledCancelbtn>Cancel</StyledCancelbtn>
-                    <StyledAddbtn onClick={submitHandler}>Add</StyledAddbtn>
-                </StyledBtnDiv>
-            </StyledContainer>
-        </>
-    );
 };
-export default AddPlant;
+
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
+ 
+
 import Drawer from './Drawer'
-<<<<<<< HEAD
 import {getPlants} from '../store/actions/index'
 
 import { useSelector, useDispatch} from 'react-redux'
@@ -8,34 +9,16 @@ import { useSelector, useDispatch} from 'react-redux'
 import PlantCard from './PlantCard';
 
 
-=======
-import { getPlants } from '../store/actions/index'
-import { useSelector, useDispatch } from 'react-redux'
-import PlantCard from './PlantCard';
->>>>>>> 3d388f5a203f1f9570089cd31271f686250d1922
-// const H1SignIn = styled.h1`
-// text-align: center;
-// font-family: Amatic SC;
-// font-size: 64px;
-// `;
-<<<<<<< HEAD
-
 
 const Dashboard = (props) => {    
     const dispatch = useDispatch();
     const plants = useSelector(state => state.plantList);
 
    
-=======
-const Dashboard = (props) => {
-    const dispatch = useDispatch();
-    const plants = useSelector(state => state.plantList);
->>>>>>> 3d388f5a203f1f9570089cd31271f686250d1922
     // MVP for React 1
-    useEffect(() => {
+    useEffect(()=> {
         // this will be my action to call the plants list when I get the end point
         dispatch(getPlants());
-<<<<<<< HEAD
         
     }, [dispatch])
 
@@ -45,32 +28,20 @@ const Dashboard = (props) => {
             <div> 
                 {plants.map(plant => 
                 <div>
-                    <PlantCard id={plant.id}
+                    <PlantCard 
                     nickname = { plant.nickname }
                     species = { plant.species }
                     image = { plant.image } 
                     h2o={plant.h2o_frequency}/>
                 </div> 
                              
-=======
-    }, [dispatch])
-    return (
-        <section id="dashBg">
-            <Drawer />
-            <div>
-                {plants.map(plant =>
-                    <div>
-                        <PlantCard
-                            nickname={plant.nickname}
-                            species={plant.species}
-                            image={plant.image}
-                            h2o={plant.h2o_frequency} />
-                    </div>
->>>>>>> 3d388f5a203f1f9570089cd31271f686250d1922
                 )}
                 )}
             </div>
         </section>
     )
+
 }
+        
+
 export default Dashboard;
