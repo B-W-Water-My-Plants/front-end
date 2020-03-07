df0ee2063faf6919468fa62d62d569f43dc66a45
Frances Hampton <bethhampton@Francess-MBP.attlocal.net>
2020-03-07 00:41:54 -0600

    removed import styled-components-francesHampton

--- src/components/UpdatePlant.js
+++ src/components/UpdatePlant.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 
-import styled from "styled-components";
+
 import {useDispatch, useSelector} from 'react-redux'
 import {useParams} from 'react-router-dom'
 
